d61c44fafe60d01e3d497b5e6301f2833015c064
Nkyo Lio <nkyoli@gmail.com>
2020-07-01 14:40:13 -0400

    last update

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,3 +1,32 @@
 function compute(expression) {
-    // TODO - write method definition here
+
+let operators = new Array();
+for(let i = 0; i < expression.split(/[^\D]/).length; i++) {
+    if(expression.split(/[^\D]/)[i] != "") operators.push(expression.split(/[^\D]/)[i]);
+}
+
+let numbers = expression.split(/\D/);
+for(let i = 0; i < numbers.length; i++) {
+    numbers[i] = parseInt(numbers[i]);
+}
+
+let result = numbers[0];
+
+for(let i = 0; i < operators.length; i++) {
+    result = calculation(operators[i], result, numbers[i + 1]);
+}
+return result; 
+}
+
+function calculation(operator, operant1, operant2) {
+    switch(operator) {
+        case "+":
+            return operant1 + operant2;
+        case "-":
+            return operant1 - operant2;
+        case "*":
+            return operant1 * operant2;
+        case "/":
+            return operant1 / operant2;
+    }
 }
